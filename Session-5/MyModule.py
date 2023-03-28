4fdacf8b0104fb333bcb150f319c55a3b8fd5241
pankkap <pankkap@gmail.com>
2023-03-28 12:50:36 +0530

    Session-5

---
+++ Session-5/MyModule.py
@@ -0,0 +1,14 @@
+def addition(x,y):
+    print(x+y)
+
+def subtraction(x,y):
+    print(x-y)
+
+myData = {
+    "name":"Ingenuity",
+    "location":"sector-144",
+    "EmpCount":550
+}
+
+
+companyName = "Ingenuity Gaming"
