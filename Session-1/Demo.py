9fb159a32cdabeafb8d5705fda821593878798f3
pankkap <pankkap@gmail.com>
2023-03-29 12:37:29 +0530

    no change

--- Session-1/Demo.py
+++
@@ -1,6 +0,0 @@
-x = 10
-y = 20
-z = x + y
-print("Sum of Two numbers" , z)
-
- 
--- Session-2/3-Casting.py
+++
@@ -1,7 +0,0 @@
-num1 = input("Enter First Number ")
-num1 = int(num1)
-num2 = int(input("Enter Second Number "))
-z = num1 + num2
-z = float(z)
-print(z)
-print(type(z))
